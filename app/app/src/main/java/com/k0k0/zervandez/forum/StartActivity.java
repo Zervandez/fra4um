7448ebf070271a347c3ca093d4141798675d5651
Zervandez <arielhalilaj@live.com>
2020-12-20 15:43:12 +0100

    StartActivity java internal workings.

--- app/app/src/main/java/com/k0k0/zervandez/forum/StartActivity.java
+++ app/app/src/main/java/com/k0k0/zervandez/forum/StartActivity.java
@@ -2,14 +2,37 @@ package com.k0k0.zervandez.forum;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class StartActivity extends AppCompatActivity {
+
+    // Thy be buttons!
+    private Button loginBtn, registerBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
 
+        loginBtn = findViewById(R.id.start_loginBtn);           // connect the variable to the corresponding xml view
+        registerBtn = findViewById(R.id.start_registerBtn);
+
+        // start new activity after click (login)
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(StartActivity.this, LoginActivity.class));
+            }
+        });
+
+        registerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(StartActivity.this, RegisterActivity.class));
+            }
+        });
     }
 }
