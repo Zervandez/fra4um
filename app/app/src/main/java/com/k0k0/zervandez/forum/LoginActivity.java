a7b92da256483b4a9b9444f979abafae143de8b0
Zervandez <arielhalilaj@live.com>
2020-12-22 19:37:07 +0100

    Update LoginActivity.java

--- app/app/src/main/java/com/k0k0/zervandez/forum/LoginActivity.java
+++ app/app/src/main/java/com/k0k0/zervandez/forum/LoginActivity.java
@@ -3,13 +3,18 @@ package com.k0k0.zervandez.forum;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
 
     private EditText emailET, passwordET;
     private Button loginBtn;
+
+    private FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +24,21 @@ public class LoginActivity extends AppCompatActivity {
         emailET = findViewById(R.id.login_emailEditText);
         passwordET = findViewById(R.id.login_editTextPassword);
         loginBtn = findViewById(R.id.login_loginBtn);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String emailStr = emailET.getText().toString().trim();
+                String passStr = passwordET.getText().toString();
+                loginUser(emailStr, passStr);
+            }
+        });
+
+    }
+
+    private void loginUser(String email, String pass) {
         
     }
 }
