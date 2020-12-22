1caa86fae8d1b1bcf83873d5af1d20bbc47b36f3
Zervandez <arielhalilaj@live.com>
2020-12-22 20:11:31 +0100

    iOS
    Just for fun.

---
+++ iOS/fra4um/fra4um/ContentView.swift
@@ -0,0 +1,38 @@
+//
+//  ContentView.swift
+//  fra4um
+//
+//  Created by Ariel Halilaj on 22.12.20.
+//  Copyright © 2020 Ariel Halilaj. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        //Text("Hello, World!")
+        VStack {
+            Button(action: {
+                print("Regster btn clicked")
+            }) {
+                Text("Register")
+            }
+            
+            Button(action: {
+                print("Login btn clicked")
+            }) {
+                Text("Login")
+                    .frame(width: 300.0, height: 34.0)
+            }
+            
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+
