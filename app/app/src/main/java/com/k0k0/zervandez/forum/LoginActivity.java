3c4346b79ca14326fde38d6863bb3402d4c9a3f6
Zervandez <arielhalilaj@live.com>
2020-12-22 19:32:18 +0100

    LoginActivity update.

--- app/app/src/main/java/com/k0k0/zervandez/forum/LoginActivity.java
+++ app/app/src/main/java/com/k0k0/zervandez/forum/LoginActivity.java
@@ -3,12 +3,22 @@ package com.k0k0.zervandez.forum;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText emailET, passwordET;
+    private Button loginBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        emailET = findViewById(R.id.login_emailEditText);
+        passwordET = findViewById(R.id.login_editTextPassword);
+        loginBtn = findViewById(R.id.login_loginBtn);
+        
     }
 }
