0add6cea7f7be1b220c88dc74429aa0987ed1f8d
Zervandez <arielhalilaj@live.com>
2020-12-23 20:42:30 +0100

    Logout from FeedActivity

--- app/app/src/main/java/com/k0k0/zervandez/forum/FeedActivity.java
+++ app/app/src/main/java/com/k0k0/zervandez/forum/FeedActivity.java
@@ -2,28 +2,48 @@ package com.k0k0.zervandez.forum;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;       // apparently, we're old fashioned
 import com.google.android.material.snackbar.Snackbar;                               // we wear it with pride, baby
+import com.google.firebase.auth.FirebaseAuth;
+
 
 public class FeedActivity extends AppCompatActivity {
+
+    private FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feed);
 
-
-
         FloatingActionButton fab = findViewById(R.id.fab);
+
+        auth = FirebaseAuth.getInstance();
+
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Here's a Snackbar", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "Speak Socrates!", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+                startActivity(new Intent(FeedActivity.this, PostActivity.class));
             }
+        });
+
+        fab.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View view) {
+                auth.signOut();
+                Toast.makeText(getApplicationContext(), "LOGGED OUT", Toast.LENGTH_LONG).show();
+                startActivity(new Intent(FeedActivity.this, StartActivity.class));
+                finish();
+                return false;
+            }
+
         });
     }
 }
---
+++ app/app/src/main/java/com/k0k0/zervandez/forum/PostActivity.java
@@ -0,0 +1,14 @@
+package com.k0k0.zervandez.forum;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PostActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_post);
+    }
+}
