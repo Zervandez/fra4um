bb9547305a309b735798c41c6a587ccb723a845f
Zervandez <arielhalilaj@live.com>
2020-12-25 21:46:19 +0100

    LayoutInflater class

--- app/app/src/main/java/com/k0k0/zervandez/forum/FeedActivity.java
+++ app/app/src/main/java/com/k0k0/zervandez/forum/FeedActivity.java
@@ -3,11 +3,15 @@ package com.k0k0.zervandez.forum;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.provider.ContactsContract;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -84,6 +88,37 @@ public class FeedActivity extends AppCompatActivity {
             }
         });
 
+    class CustomListRowAdapter extends BaseAdapter {
 
+        Context context;
+        String[] data;
+        private LayoutInflater inflater = null;
+
+        public CustomListRowAdapter(Context context, String[] data) {
+            this.context = context;
+            this.data = data;
+            inflater = (LayoutInflater) context.getSystemService(context.LAYOUT_INFLATER_SERVICE);
+        }
+
+        @Override
+        public int getCount() {
+            return 0;
+        }
+
+        @Override
+        public Object getItem(int i) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int i, View view, ViewGroup viewGroup) {
+            return null;
+        }
+    }
     }
 }
