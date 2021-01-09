ad70f562154f850b0a8ff83c4419a546bd6862bb
jdrandomperson <33594579+jdrandomperson@users.noreply.github.com>
2021-01-09 16:53:26 +0100

    RegisterActivity Comments

--- app/app/src/main/java/com/k0k0/zervandez/forum/RegisterActivity.java
+++ app/app/src/main/java/com/k0k0/zervandez/forum/RegisterActivity.java
@@ -18,12 +18,6 @@ import com.google.firebase.auth.FirebaseAuth;
 
 /**
  * Allows the user to create an account via their email address.
- * <p> The Main components that will be showed to the user are:
- * <ul>
- * <li> A block for the user's email address
- * <li> A block for the password
- * <li> A button to finish the registration
- * </ul>
  *
  * @author Ariel Halilaj
  * @since  10.12.2020
@@ -37,6 +31,17 @@ public class RegisterActivity extends AppCompatActivity {
 
     private FirebaseAuth firebaseAuth;     //  we declare an instance of Firebase Auth
 
+    /**
+     * Interact with the Register-interface.
+     * <p> The Main components of the Register-interface are:
+     * <ul>
+     *  <li> A block for the user's email address
+     *  <li> A block for the password
+     *  <li> A button to finish the registration
+     * </ul>
+     * @param savedInstanceState
+     */
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -49,6 +54,13 @@ public class RegisterActivity extends AppCompatActivity {
         firebaseAuth = FirebaseAuth.getInstance();      // we initialise the instance
 
         registerBtn.setOnClickListener(new View.OnClickListener() {
+            /**
+             * Get the required information from the user (email, password).
+             * <p> If the user hasn't filled the email block, he/she will get a notification
+             * <p> If the user hasn't filled the password block, he/she will get a notification
+             * <p> If the two required information have been given, they will be passed to the registerUser(email, pass)-Method to perform the authentication
+             * @param view
+             */
             @Override
             public void onClick(View view) {
                 String emailStr = emailET.getText().toString().trim();
@@ -63,6 +75,19 @@ public class RegisterActivity extends AppCompatActivity {
             }
         });
     }
+
+    /**
+     * Perform the authentication.
+     * <p>
+     *     If the user has provided correct information, the registration process will be successful and he/she will be directed to the Login-interface
+     *     so that he/she can login to the system
+     * <p>
+     *     If the user has provided false information, the registration process will be failed and he/she will stay in the Register-Interface until he/she
+     *     provides correct information
+     *
+     * @param email The email address of the user
+     * @param pass  A Password
+     */
 
     private void registerUser(String email, String pass) {
         firebaseAuth.createUserWithEmailAndPassword(email, pass).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
