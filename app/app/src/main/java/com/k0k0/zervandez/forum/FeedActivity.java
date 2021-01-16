dea8e5a9845909960f5ad078c47e99a7137646bf
jdrandomperson <33594579+jdrandomperson@users.noreply.github.com>
2021-01-16 12:36:29 +0100

    Comment

--- app/app/src/main/java/com/k0k0/zervandez/forum/FeedActivity.java
+++ app/app/src/main/java/com/k0k0/zervandez/forum/FeedActivity.java
@@ -27,6 +27,7 @@ import com.google.firebase.database.ValueEventListener;
 import java.util.ArrayList;
 import java.util.List;
 
+// I see nothing //
 
 /*
 class CustomListRowAdapter<String> extends BaseAdapter {
